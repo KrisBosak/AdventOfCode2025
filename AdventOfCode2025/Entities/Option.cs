6db347d03ffa80de143be203f7d3f4ceb4e654ac
Kris <kris.bosak96@gmail.com>
2025-12-03 18:26:35 +0100

    - Code cleanup - Add initial menu

---
+++ AdventOfCode2025/Entities/Option.cs
@@ -0,0 +1,9 @@
+namespace AdventOfCode2025.Entities
+{
+    public class Option(string name, Action selected)
+    {
+        public string Name { get; } = name;
+        public Action Selected { get; } = selected;
+    }
+}
+    
---
+++ AdventOfCode2025/MainProgramHelpers.cs
@@ -0,0 +1,85 @@
+﻿using AdventOfCode2025.Entities;
+
+namespace AdventOfCode2025
+{
+    public static class MainProgramHelpers
+    {
+        public static readonly  List<Option> _options =
+        [
+            new Option("Day 1", () => WriteTemporaryMessage("Day 1")),
+            new Option("Day 2", () => WriteTemporaryMessage("Day 2")),
+            new Option("Day 3", () => WriteTemporaryMessage("Day 3")),
+            new Option("Exit", () => Environment.Exit(0)),
+        ];
+
+        public static void HandleMenuNavigation()
+        {
+            // Menu position
+            int index = 0;
+
+            WriteMenu(_options[index]);
+
+            ConsoleKeyInfo keyInfo;
+            do
+            {
+                keyInfo = Console.ReadKey();
+
+                switch (keyInfo.Key)
+                {
+                    case ConsoleKey.DownArrow:
+                    {
+                        if (index + 1 < _options.Count)
+                        {
+                            index++;
+                            WriteMenu(_options[index]);
+                        }
+
+                        break;
+                    }
+                    case ConsoleKey.UpArrow:
+                    {
+                        if (index - 1 >= 0)
+                        {
+                            index--;
+                            WriteMenu(_options[index]);
+                        }
+
+                        break;
+                    }
+                    case ConsoleKey.Enter:
+                        _options[index].Selected.Invoke();
+                        index = 0;
+                        break;
+                }
+            }
+            while (keyInfo.Key is not ConsoleKey.X);
+        }
+
+        static void WriteMenu(Option selectedOption)
+        {
+            Console.Clear();
+
+            foreach (Option option in _options)
+            {
+                if (option == selectedOption)
+                {
+                    Console.Write("> ");
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+
+                Console.WriteLine(option.Name);
+            }
+        }
+
+        static void WriteTemporaryMessage(string message)
+        {
+            Console.Clear();
+            Console.WriteLine(message);
+            Thread.Sleep(3000);
+            WriteMenu(_options!.First());
+        }
+    }
+}
